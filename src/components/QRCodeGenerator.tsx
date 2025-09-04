945400e2ef32a611392079430547778f46faa33e
karthikeyan_p <dkarthideepak@gmail.com>
2025-09-04 06:54:38 +0530

    Update QRCodeGenerator.tsx

--- src/components/QRCodeGenerator.tsx
+++ src/components/QRCodeGenerator.tsx
@@ -7,7 +7,7 @@ const QRCodeGenerator: React.FC = () => {
   useEffect(() => {
     const generateQR = async () => {
       try {
-        const url = `https://myapp.com/wish-form`;
+        const url = `/wish-form`;
         const qrData = await QRCode.toDataURL(url, {
           width: 256,
           margin: 2,
